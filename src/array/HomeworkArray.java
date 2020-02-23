49db3982c98054043c2a0b739a33d903f1060dff
Danil Shpakov <danilyh7@gmail.com>
2020-02-24 00:02:41 +0600

    Подготовил класс для домашки по массивам.

---
+++ src/array/HomeworkArray.java
@@ -0,0 +1,13 @@
+/*
+Создайте массив из 5 чисел типа double.
+Используя цикл while, выведите сумму всех чисел массива.
+Используя цикл for, выведите произведение всех чисел массива.
+ */
+
+package array;
+
+public class HomeworkArray {
+    public static void main(String[] args) {
+
+    }
+}
