9a0e3564fdb3c0777ab7a95c30687c94525fa3d7
Danil Shpakov <tengridanil@gmail.com>
2020-02-24 13:39:10 +0600

    Домашка по массивам

--- src/array/HomeworkArray.java
+++ src/array/HomeworkArray.java
@@ -8,6 +8,21 @@ package array;
 
 public class HomeworkArray {
     public static void main(String[] args) {
-
+//      Создайте массив из 5 чисел типа double.
+        double[] d_arr = {1, 2, 3, 4, 5};
+        double while_sum = 0;
+//      Используя цикл while, выведите сумму всех чисел массива.
+        int i = 0;
+        while (i < d_arr.length) {
+            while_sum += d_arr[i];
+            i++;
+        }
+        System.out.println("Сумма всех чисел массива = " + while_sum);
+//      Используя цикл for, выведите произведение всех чисел массива.
+        double product_numbers = 1;
+        for (int x = 0; x < d_arr.length; x++) {
+            product_numbers *= d_arr[x];
+        }
+        System.out.println("Произведение всех чисел массива = " + product_numbers);
     }
 }
