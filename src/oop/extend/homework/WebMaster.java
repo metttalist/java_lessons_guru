995ccb62840efdaf27c7461c655c6f882f2087fb
Danil Shpakov <tengridanil@gmail.com>
2020-02-26 18:22:13 +0600

    ООП. Продолжение домашки по наследованию.

--- src/oop/extend/homework/WebMaster.java
+++ src/oop/extend/homework/WebMaster.java
@@ -9,6 +9,21 @@
 
 package oop.extend.homework;
 
-public class WebMaster {
+import oop.myclass.homework_oop_first.User;
 
+public class WebMaster extends User {
+    String web_address;
+
+    public WebMaster(int id, String login, String password, String web_address) {
+        super(id, login, password);
+        this.web_address = web_address;
+    }
+
+    public String getWeb_address() {
+        return web_address;
+    }
+
+    public void setWeb_address(String web_address) {
+        this.web_address = web_address;
+    }
 }
