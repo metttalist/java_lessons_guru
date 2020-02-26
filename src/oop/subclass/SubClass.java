bac75a42c896e77a5afd73413fe91b5e57f9005d
Danil Shpakov <danilyh7@gmail.com>
2020-02-26 23:28:32 +0600

    Урок по ООП, вложенные классы.

---
+++ src/oop/subclass/SubClass.java
@@ -0,0 +1,9 @@
+package oop.subclass;
+
+public class SubClass {
+    public static void main(String[] args) {
+        Firm f = new Firm("My firm", "Dan", 30);
+        System.out.println(f.director.name);
+        System.out.println("Директор является пенсионером: " + f.director.is_pensioner());
+    }
+}
