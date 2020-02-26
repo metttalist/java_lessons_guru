89b3fa4f4c20c7079cb71f5d06f1af6c0f609e9d
Danil Shpakov <tengridanil@gmail.com>
2020-02-26 16:01:55 +0600

    ООП. Домашка по статическим модификаторам доступа.

---
+++ src/oop/static_data/homework/MyMathTest.java
@@ -0,0 +1,10 @@
+package oop.static_data.homework;
+
+public class MyMathTest {
+    public static void main(String[] args) {
+//      Проверьте все 4 метода, выводя результат их работы в консоль.
+        System.out.println(MyMath.getCalls());
+        System.out.println(MyMath.add());
+        System.out.println(MyMath.getCalls());
+    }
+}
