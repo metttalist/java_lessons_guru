a007aaa24ef5ed1118aec9725cbb0101713808fb
Danil Shpakov <tengridanil@gmail.com>
2020-02-26 11:59:49 +0600

    Домашка по ООП. Перегрузка.

--- src/oop/myclass/homework_oop_first/MyClass.java
+++ src/oop/myclass/homework_oop_first/MyClass.java
@@ -6,9 +6,23 @@ public class MyClass {
         User[] arr_user = new User[2];
         arr_user[0] = new User(123, "Dan", "Dan1");
         arr_user[1] = new User(321, "Ban", "Dan2");
+
 //      Выведите в консоль все поля обоих объектов в следующем виде (можно с использованием массива и цикла): «id=5; логин=admin; password=abc»
         for (User temp : arr_user){
             System.out.println("id=" + temp.id + "; " + "логин=" + temp.login + "; " + "password=" + temp.password);
         }
+
+//      Создайте объект класса User.
+        User dan = new User(1, "Dan", "Dan login");
+        System.out.println(dan.get_info());
+
+//      Воспользуйтесь методами set и get для установки новых значения для каждого поля и получения результат.
+        dan.setId(2);
+        dan.setLogin("Дэн");
+        dan.setPassword("789");
+        System.out.println(dan.getId());
+        System.out.println(dan.getLogin());
+        System.out.println(dan.getPassword());
+
     }
 }
--- src/oop/myclass/homework_oop_first/User.java
+++ src/oop/myclass/homework_oop_first/User.java
@@ -1,10 +1,20 @@
 /*
+№ 1
 Создайте класс User со следующими полями, подобрав правильный тип данных: id, логин, пароль.
 Создайте конструктор класса User, приняв все 3 параметра для полей.
 Присвойте полям полученные параметры.
 Создайте два объекта класса User с различными входными параметрами.
 Выведите в консоль все поля обоих объектов в следующем виде (можно с использованием массива и цикла): «id=5; логин=admin; password=abc»
 Примечание: id – это уникальный идентификатор. Он всегда является только целым положительным числом.
+ */
+
+/*
+№ 2
+Используя класс User из предыдущего упражнения, добавьте в нём методы get и set для всех полей.
+Добавьте ещё один метод set для каждого поля (сделав, по сути, перегрузку методов),но уже без параметра.
+Внутри метода должно присваиваться значение по умолчанию (id = 0; login=”guest”; password=””).
+Создайте объект класса User.
+Воспользуйтесь методами set и get для установки новых значения для каждого поля и получения результат.
  */
 
 package oop.myclass.homework_oop_first;
@@ -21,4 +31,46 @@ public class User {
         this.login = login;
         this.password = password;
     }
+
+//  Используя класс User, добавьте в нём методы get и set для всех полей.
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin(){
+        return login;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+/*
+Добавьте ещё один метод set для каждого поля (сделав, по сути, перегрузку методов),но уже без параметра.
+Внутри метода должно присваиваться значение по умолчанию (id = 0; login=”guest”; password=””).
+ */
+    public void setId(){
+        setId(0);
+    }
+
+    public void setLogin(){
+        setLogin("guest");
+    }
+
+    public void setPassword(){
+        this.password = "";
+    }
+
+    public String get_info(){ return "Пользователь с логин: " + this.login + "; " + "id=" + this.id + "; " + " Пароль: " + this.password;}
 }
