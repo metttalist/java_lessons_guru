99f1da82b9359170ee3db661eff718742889bbd2
Danil Shpakov <tengridanil@gmail.com>
2020-03-03 13:39:39 +0600

    Урок по обобщенным классам и методам (ООП).

--- src/oop/generic/Generic.java
+++ src/oop/generic/Generic.java
@@ -2,6 +2,15 @@ package oop.generic;
 
 public class Generic {
     public static void main(String[] args) {
-        
+//      Создание переменной с указанными параметрами(в квадратных скобках < >)
+        Test <Integer, String> t = new Test<Integer, String>(10, "Привет");
+        t.print();
+        t.setData_1(11);
+        System.out.println(t.getData_1());
+        t.setData_2("Дэнчик");
+        System.out.println(t.getData_2());
+//      Создание переменной без указания параметров
+        Test t1 = new Test(true, false);
+        t1.print();
     }
 }
--- src/oop/generic/Test.java
+++ src/oop/generic/Test.java
@@ -1,32 +1,34 @@
 package oop.generic;
 
+//В угловых скобках указываются некие типы данных
 public class Test<X, Y> {
     private X data_1;
     private Y data_2;
 
-    public Test(X data_1, Y data_2){
+    //  в крнстркуторе получаем какие-то данные
+    public Test(X data_1, Y data_2) {
         this.data_1 = data_1;
         this.data_2 = data_2;
     }
 
-    public void print(){
+    public void print() {
         System.out.println(data_1);
         System.out.println(data_2);
     }
 
-    public X getData_1(){
+    public X getData_1() {
         return data_1;
     }
 
-    public Y getData_2(){
+    public Y getData_2() {
         return data_2;
     }
 
-    public void setData_1(X data_1){
+    public void setData_1(X data_1) {
         this.data_1 = data_1;
     }
 
-    public void setData_2(Y data_2){
+    public void setData_2(Y data_2) {
         this.data_2 = data_2;
     }
 
