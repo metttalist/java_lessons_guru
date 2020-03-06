f9ee1bb677c6d537aed6e6758f0d50a117098ede
Danil Shpakov <tengridanil@gmail.com>
2020-03-06 12:46:20 +0600

    Домашка по теме рекурсия. Не решена.

---
+++ src/oop/recursion/Homework.java
@@ -0,0 +1,16 @@
+/*
+Используя рекурсию, выведите все числа от 1 до 100.
+Используя рекурсию, найдите сумму цифр произвольного целого числа (для примера, можете взять 1539).
+ */
+
+package oop.recursion;
+
+public class Homework {
+//    public static int hundred(int x){
+////        return hundred(x);
+//    }
+
+    public static void main(String[] args) {
+//        System.out.println(hundred(100));
+    }
+}
