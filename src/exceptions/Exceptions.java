3d2adab04058eb9f9bac3b1fcfb1a63d4350364b
Danil Shpakov <tengridanil@gmail.com>
2020-03-06 13:23:17 +0600

    Урок по теме исключения.

---
+++ src/exceptions/Exceptions.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+import javax.swing.*;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        String in_1 = JOptionPane.showInputDialog(null, "Введите число 1");
+        String in_2 = JOptionPane.showInputDialog(null, "Введите число 2");
+//        Попытка выполнения кода
+        try {
+            int a = Integer.parseInt(in_1);
+            int b = Integer.parseInt(in_2);
+            JOptionPane.showMessageDialog(null, "Сумма чисел: " + (a + b));
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Вы не ввели число");
+        }
+
+    }
+}
